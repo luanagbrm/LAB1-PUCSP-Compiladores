2912089395a3e64df3137eda2461612bdfb27905
Luana Macedo <92169736+luanagbrm@users.noreply.github.com>
2026-04-03 00:50:15 -0300

    remove unused code on scanner

--- Atividade1/scanner_simples.sh
+++ Atividade1/scanner_simples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read -r linha; do
-    linha_limpa=$(echo "$linha" | sed 's/\xc2\xa0//g' | tr -d ' \t\r')
+    linha_limpa=$(echo "$linha" | tr -d ' \t\r')
     if [ ! -z "$linha_limpa" ]; then
         echo "[SCANNER] Linha recebida: '$linha_limpa'"
     fi
